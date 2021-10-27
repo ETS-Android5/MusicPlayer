65365b52f06fc2d1d5d5d062bf9cd301c1460156
erwinmqiu <79524653+erwinmqiu@users.noreply.github.com>
2021-10-28 01:34:15 +1100

    Version 0.8 (Early Build)
    Functional:
    - Basic Design
    - RecyclerView Implementation
    - Sort and Search Function
    - Description in DetailActivity
    - Search Functionality in Google on MainActivity
    - Album Art in DetailActivity
    - Database Functionality is working
    - Song Player Working
    - Users can update the rating in the Database completely
    - Program Tracks Number of Times User Listened to a Song
    - Allow User to Sort by Most Played Song, Rating, etc. Genre
    - Fix Behaviour of Rewind and Fast Forward Buttons when user clicks Pause
    
    Priority Backlog:
    - Fix Patchy audio with Media Player
    - UI Fixing and Polishing
    - Dark Mode
    
    Additional Features if Time Allows:
    - Detail Activity: "Similar Songs... feature"

--- app/src/main/java/au/edu/unsw/infs3634/musicrecommender/DetailActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/musicrecommender/DetailActivity.java
@@ -249,15 +249,16 @@ public class DetailActivity extends AppCompatActivity {
                 //Get current position
                 int position = mediaPlayerCurrent.getCurrentPosition();
 
-                //Show the pause button but hide the play button
-                btnPause.setVisibility(View.VISIBLE);
-                btnPlay.setVisibility(View.GONE);
+
 
                 //If the song is at least 5 seconds in, we can rewind back by 5 seconds
                 if (position > 5000) {
                     position -= 5000;
 
                     //Set the current progress on seekBar and change the TextView to the position of the player
+                    //Show the pause button but hide the play button
+                    btnPause.setVisibility(View.VISIBLE);
+                    btnPlay.setVisibility(View.GONE);
                     mediaPlayerCurrent.start();
                     mediaPlayerCurrent.seekTo(position);
                     playerPosition.setText(convertFormat(position));
